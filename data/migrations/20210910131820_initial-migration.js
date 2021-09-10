992f00610368bf838aea4d1f2717fa3d1757dc52
dominthebox <dsallustro@gmail.com>
2021-09-10 09:39:31 -0400

    wip, recources table created

--- data/migrations/20210910131820_initial-migration.js
+++ data/migrations/20210910131820_initial-migration.js
@@ -1,16 +1,20 @@
 exports.up = function(knex) {
     return knex.schema
-        .createTable('projects', table => {
-            table.increments('project_id')
-            table.string('project_name', 128)
+        .createTable('projects', column => {
+            column.increments('project_id')
+            column.string('project_name', 128)
                 .notNullable()
                 .unique()
-            table.string('project_description', 255)
-            table.boolean('project_completed')
+            column.string('project_description', 255)
+            column.boolean('project_completed')
                 .defaultTo(false)
         })
-        .createTable('resources', table => {
-            table.increments('resource_id')
+        .createTable('resources', column => {
+            column.increments('resource_id')
+            column.string('resource_name', 128)
+                .notNullable()
+                .unique()
+            column.string('resource_description', 255)
         })
         .createTable('tasks', table => {
             table.increments('task_id')
