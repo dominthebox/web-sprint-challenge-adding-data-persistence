3f81217f9de6e078468b2f8f4ce4b46f1aa147d7
dominthebox <dsallustro@gmail.com>
2021-09-10 08:41:32 -0400

    wip, wired up index/server.js and the resource router

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,21 @@
 // build your `/api/resources` router here
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.json('getting the resources')
+})
+router.post('/', (req, res, next) => {
+    res.json('posting a resource')
+})
+
+router.use((err, req, res,  next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+        sageAdvice: 'Finding the real error is 90% of the bug fix',
+        message: err.message,
+        stack: err.stack,
+    })
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,1 +1,10 @@
 // build your server here and require it from index.js
+const express = require('express');
+
+const resourceRouter = require('./resource/router');
+const server = express();
+
+server.use(express.json());
+server.use('/api/resources', resourceRouter);
+
+module.exports = server;
